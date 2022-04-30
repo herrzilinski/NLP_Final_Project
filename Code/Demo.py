513531f4a386161365a9ff5e00e543f72af7a25e
herrzilinski <herrzilinski@gmail.com>
2022-04-30 04:32:16 -0400

    Reusable demo

--- Code/Demo.py
+++ Code/Demo.py
@@ -22,7 +22,7 @@ OR_PATH = os.getcwd()
 os.chdir("..")  # Change to the parent directory
 PATH = os.getcwd()
 DATA_DIR = os.getcwd() + os.path.sep + 'Data' + os.path.sep
-# MODEL_DIR = os.getcwd() + os.path.sep + 'Models' + os.path.sep
+MODEL_DIR = os.getcwd() + os.path.sep + 'Models' + os.path.sep
 
 os.chdir(OR_PATH)  # Come back to the folder where the code resides , all files will be left on this directory
 
@@ -30,16 +30,13 @@ os.chdir(OR_PATH)  # Come back to the folder where the code resides , all files 
 # df_train_raw = pd.read_csv(DATA_DIR+'train.csv')
 df_raw = pd.read_csv(DATA_DIR + 'train.csv')
 label_names = df_raw.columns[2:]
-df = df_raw.copy()
-df['labels'] = df_raw[label_names].values.tolist()
-df2 = df[['comment_text', 'labels']]
+# df = df_raw.copy()
+# df['labels'] = df_raw[label_names].values.tolist()
+# df2 = df[['comment_text', 'labels']]
 OUTPUTS_a = len(label_names)
 
 # %%
-
-
-#####  Not gonna need anything above this it runs smoothly ####
-
+####  Not gonna need anything above this it runs smoothly ####
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -48,23 +45,19 @@ tokenizer = ElectraTokenizer.from_pretrained('google/electra-small-discriminator
 NICKNAME = 'Electra'
 
 
-
 # %%
 class CustomDataset(Dataset):
 
-    def __init__(self, dataframe, tokenizer, max_len):
+    def __init__(self, text, tokenizer, max_len):
         self.tokenizer = tokenizer
-        self.data = dataframe
-        self.comment_text = dataframe['comment_text']
-        self.targets = self.data['labels']
+        self.comment_text = text
         self.max_len = max_len
 
     def __len__(self):
         return len(self.comment_text)
 
     def __getitem__(self, index):
-        comment_text = str(self.comment_text[index])
-        comment_text = " ".join(comment_text.split())
+        comment_text = " ".join(self.comment_text.split())
 
         inputs = self.tokenizer.encode_plus(
             comment_text,
@@ -84,7 +77,7 @@ class CustomDataset(Dataset):
             'ids': torch.tensor(ids, dtype=torch.long),
             'mask': torch.tensor(mask, dtype=torch.long),
             'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
-            'targets': torch.tensor(self.targets[index], dtype=torch.float)
+            # 'targets': torch.tensor(self.targets[index], dtype=torch.float)
         }
 
 
@@ -96,46 +89,69 @@ class ElectraClass(torch.nn.Module):
         self.l1 = ElectraModel.from_pretrained('google/electra-small-discriminator', return_dict=False)
         self.l2 = torch.nn.Dropout(0.3)
         self.l3 = torch.nn.Linear(256, OUTPUTS_a)
+        self.act = torch.nn.Sigmoid()
 
     def forward(self, ids, mask, token_type_ids):
         output_1 = self.l1(ids, attention_mask=mask, token_type_ids=token_type_ids)
         output_2 = self.l2(output_1[0])
         output = self.l3(output_2)
-        return output.mean(1)
+        pool = output.mean(1)
+        return self.act(pool)
 
 
-model1 = ElectraClass()
+model = ElectraClass()
 model2 = ElectraClass()
 
-#model1.to(device)
-model1.load_state_dict(torch.load('/home/ubuntu/NLP/Final Project/NLP_Final_Project/Code/model1.pt'))
-model1.eval()
-model2.eval()
+# model1.to(device)
+model.load_state_dict(torch.load(MODEL_DIR + 'model_Electra_1.pt'))
+model.eval()
+# model2.eval()
 
-# This is just to test, we will put the input in this dataframe format and muse fix labels ex: [0,0,0,0,0]
-df3 = df2.iloc[6:7,:]
-df3.reset_index(inplace=True, drop=True)
-
-testing_set = CustomDataset(df3, tokenizer, 200)
-testing_loader = DataLoader(testing_set, batch_size=1)
-
-for samp in testing_loader:
-    break
-
-ids = samp['ids']                           #.to(device, dtype=torch.long)
-mask = samp['mask']                         #.to(device, dtype=torch.long)
-token_type_ids = samp['token_type_ids']     #.to(device, dtype=torch.long)
-
-outputs1 = model1(ids, mask, token_type_ids)
-outputs2 = model2(ids, mask, token_type_ids)
+input1 = 'COCKSUCKER BEFORE YOU PISS AROUND ON MY WORK'  # 1,1,1,0,1,0
+input2 = "Your vandalism to the Matt Shirvington article has been reverted.  " \
+         "Please don't do it again, or you will be banned."  # 0,0,0,0,0,0
+input3 = "Please stop. If you continue to ignore our policies by introducing " \
+         "inappropriate pages to Wikipedia, you will be blocked."  # 0,0,0,1,0,0
+input4 = "User:NHRHS2010 is a homo like mitt romney is. "  # 1,0,1,0,1,1
+input5 = "There is a special place reserved for you in hell, you nazi piece of shit"  # Original Text, for result only
 
 
+# testing_set = CustomDataset(df3, tokenizer, 200)
+# testing_loader = DataLoader(testing_set, batch_size=1)
+# %%
+def toxicity_detection(text=None):
+    if text is None:
+        text = input('Please provide a message to classify:')
+    if not isinstance(text, str):
+        raise ValueError('message must be a string!')
+
+    testing_set = CustomDataset(text, tokenizer, 200)
+    testing_loader = DataLoader(testing_set, batch_size=1)
+
+    for samp in testing_loader:
+        ids = samp['ids']  # .to(device, dtype=torch.long)
+        mask = samp['mask']  # .to(device, dtype=torch.long)
+        token_type_ids = samp['token_type_ids']  # .to(device, dtype=torch.long)
+
+    outputs1 = model(ids, mask, token_type_ids)
+    predicted1 = outputs1.detach().cpu().numpy()
+    predicted1[predicted1 >= 0.5] = 1
+    predicted1[predicted1 < 0.5] = 0
+    res = [label_names[x] for x in range(6) if predicted1.flatten()[x] == 1]
+
+    if len(res) == 0:
+        print('No toxicity detected.')
+    else:
+        print(res)
+
+    # outputs2 = model2(ids, mask, token_type_ids)
+    # predicted2 = outputs2.detach().cpu().numpy()
+    # predicted2[predicted2 >= 0.5] = 1
+    # predicted2[predicted2 < 0.5] = 0
 
 
-
-
-
-
+# %%
+toxicity_detection(input4)
 
 # %%
 # num_testing_steps = EPOCHS * len(testing_loader)
@@ -175,4 +191,3 @@ outputs2 = model2(ids, mask, token_type_ids)
 #     fpr_micro, tpr_micro, _ = metrics.roc_curve(targetss.ravel(), outputs.ravel())
 #     auc_micro = metrics.auc(fpr_micro, tpr_micro)
 #     print(f"AUC Score (Micro) = {auc_micro}")
-
