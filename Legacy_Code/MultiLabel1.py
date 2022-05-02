c41d8587812bcd34e44fc36f97e58e1cb32163b3
herrzilinski <herrzilinski@gmail.com>
2022-05-02 03:28:03 -0400

    Code Cleanup

---
+++ Legacy_Code/MultiLabel1.py
@@ -0,0 +1,182 @@
+import pandas as pd
+import os
+from datasets import load_dataset
+from transformers import AutoTokenizer, DataCollatorWithPadding
+from torch.utils.data import DataLoader
+from transformers import AutoModelForSequenceClassification
+from transformers import BertTokenizer, BertModel, BertConfig
+from transformers import AdamW
+from transformers import get_scheduler
+import torch
+from tqdm.auto import tqdm
+from datasets import load_metric
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset
+from torch.nn import BCEWithLogitsLoss
+
+# References
+# 1) https://towardsdatascience.com/transformers-for-multilabel-classification-71a1a0daf5e1
+# 2) https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
+# 3)
+'''
+df = pd.read_csv('/home/ubuntu/NLP/Final Project/NLP_Final_Project/Data/train.csv')
+label_cols = df.columns[2:]
+df['labels'] = df[label_cols].values.tolist()
+
+xtr, xte, ytr, yte = train_test_split(df['comment_text'], df['labels'])
+df_tr = pd.DataFrame({'text':xtr, 'labels':ytr})
+df_te = pd.DataFrame({'text':xte, 'labels':yte})
+'''
+OR_PATH = os.getcwd()
+os.chdir("..")  # Change to the parent directory
+PATH = os.getcwd()
+DATA_DIR = os.getcwd() + os.path.sep + 'Data' + os.path.sep
+# MODEL_DIR = os.getcwd() + os.path.sep + 'Models' + os.path.sep
+
+os.chdir(OR_PATH)  # Come back to the folder where the code resides , all files will be left on this directory
+
+
+
+class DatasetLoad(Dataset):
+    def __init__(self):
+        self.df = pd.read_csv(DATA_DIR+'train.csv')
+        self.label_cols = self.df.columns[2:]
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        text = self.df['comment_text'][idx]
+        labels = self.df[self.label_cols].values.tolist()[idx]
+        sample= (text, labels)
+        #if self.transform:
+         #   sample = self.transform(sample)
+        return sample
+
+
+
+
+checkpoint = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+
+'''
+def tokenize_function(xdat, ydat):
+    lst = xdat.values.tolist()
+    tok = tokenizer(lst, truncation=True, padding=True, return_tensors='pt')
+    tok['labels'] = torch.Tensor(ydat.values.tolist())
+    return tok'''
+
+def collate(batch):
+    toks = {}
+    inputid, type_id, attention, labels = [], [], [], []
+    for txt, lbl in batch:
+        tok = tokenizer(txt, truncation=True, padding='max_length')
+        inputid.append(tok['input_ids'])
+        type_id.append(tok['token_type_ids'])
+        attention.append(tok['attention_mask'])
+        labels.append(lbl)
+        toks = {'input_ids': torch.tensor(inputid, dtype=torch.long),
+                'token_type_ids': torch.tensor(type_id, dtype=torch.long),
+                'attention_mask': torch.tensor(attention, dtype=torch.long),
+                'labels': torch.tensor(labels, dtype=torch.long)}
+    return toks
+
+a = DatasetLoad()
+
+
+train_dataloader = DataLoader(a, shuffle=True, batch_size=8, collate_fn=collate)
+
+eval_dataloader = DataLoader(a, batch_size=8, collate_fn=collate)
+
+# for batch in train_dataloader:
+#     break
+
+
+# model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=6)
+model = BertModel.from_pretrained(checkpoint, num_labels=6)
+
+# outputs = model(**batch)
+# print(outputs.loss, outputs.logits.shape)
+
+optimizer = AdamW(model.parameters(), lr=5e-5)
+loss_func = BCEWithLogitsLoss()
+#optimizer = AdamW(model.parameters(), lr=5e-5)
+
+'''
+# Forward pass for multilabel classification
+outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
+logits = outputs[0]
+loss_func = BCEWithLogitsLoss()
+loss = loss_func(logits.view(-1,num_labels),b_labels.type_as(logits).view(-1,num_labels)) #convert labels to float for calculation
+# loss_func = BCELoss()
+# loss = loss_func(torch.sigmoid(logits.view(-1,num_labels)),b_labels.type_as(logits).view(-1,num_labels)) #convert labels to float for calculation
+train_loss_set.append(loss.item())
+'''
+
+
+
+num_epochs = 3
+
+num_training_steps = num_epochs * len(train_dataloader)
+lr_scheduler = get_scheduler("linear", optimizer=optimizer,
+                              num_warmup_steps=0, num_training_steps=num_training_steps)
+print(num_training_steps)
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+
+class BERTFineTune(torch.nn.Module):
+    def __init__(self):
+        super(BERTFineTune, self).__init__()
+        self.l1 = BertModel.from_pretrained('bert-base-uncased')
+        self.l2 = torch.nn.Dropout(0.5)
+        self.l3 = torch.nn.Linear(768, 6)
+
+    def forward(self, input_ids, attention_mask, token_type_ids):
+        _, x1 = self.l1(input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+        x2 = self.l2(x1)
+        output = self.l3(x2)
+        return output
+
+
+model = BERTFineTune()
+model.to(device)
+device
+
+progress_bar = tqdm(range(num_training_steps))
+
+train_loss_set = []
+model.train()
+for epoch in range(num_epochs):
+    for batch in train_dataloader:
+        # batch = {k: v.to(device, dtype=torch.long) for k, v in batch.items()}
+        ids = batch['input_ids'].to(device, dtype=torch.long)
+        masks = batch['attention_mask'].to(device, dtype=torch.long)
+        tokid = batch['token_type_ids'].to(device, dtype=torch.long)
+        labels = batch.pop('labels')
+        outputs = model(ids, masks, tokid)
+        logits = outputs[0]
+        loss = loss_func(logits, labels.type_as(logits))
+        train_loss_set.append(loss.item())
+        loss.backward()
+
+        optimizer.step()
+        lr_scheduler.step()
+        optimizer.zero_grad()
+        progress_bar.update(1)
+
+'''
+metric = load_metric("glue", "mrpc")
+model.eval()
+for batch in eval_dataloader:
+    batch = {k: v.to(device) for k, v in batch.items()}
+    with torch.no_grad():
+        outputs = model(**batch)
+
+    logits = outputs.logits
+    predictions = torch.argmax(logits, dim=-1)
+    metric.add_batch(predictions=predictions, references=batch["labels"])
+
+metric.compute()
+'''
---
+++ Legacy_Code/new.py
@@ -0,0 +1,196 @@
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+import transformers
+import torch
+from torch.utils.data import Dataset, DataLoader, RandomSampler, SequentialSampler
+from transformers import BertTokenizer, BertModel, BertConfig, DistilBertModel, DistilBertTokenizer
+from tqdm.auto import tqdm
+import os
+
+OR_PATH = os.getcwd()
+os.chdir("..")  # Change to the parent directory
+PATH = os.getcwd()
+DATA_DIR = os.getcwd() + os.path.sep + 'Data' + os.path.sep
+# MODEL_DIR = os.getcwd() + os.path.sep + 'Models' + os.path.sep
+
+os.chdir(OR_PATH)  # Come back to the folder where the code resides , all files will be left on this directory
+
+
+# %%
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+MAX_LEN = 200
+TRAIN_BATCH_SIZE = 40
+VALID_BATCH_SIZE = 40
+EPOCHS = 1
+LEARNING_RATE = 1e-05
+# tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+
+
+# %%
+df_train_raw = pd.read_csv(DATA_DIR+'train.csv')
+label_names = df_train_raw.columns[2:]
+df_train = df_train_raw.copy()
+df_train['labels'] = df_train_raw[label_names].values.tolist()
+df_train = df_train[['comment_text', 'labels']]
+
+# %%
+class CustomDataset(Dataset):
+
+    def __init__(self, dataframe, tokenizer, max_len):
+        self.tokenizer = tokenizer
+        self.data = dataframe
+        self.comment_text = dataframe.comment_text
+        self.targets = self.data['labels']
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.comment_text)
+
+    def __getitem__(self, index):
+        comment_text = str(self.comment_text[index])
+        comment_text = " ".join(comment_text.split())
+
+        inputs = self.tokenizer.encode_plus(
+            comment_text,
+            None,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            pad_to_max_length=True,
+            return_token_type_ids=False
+        )
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        # token_type_ids = inputs["token_type_ids"]
+
+
+        return {
+            'ids': torch.tensor(ids, dtype=torch.long),
+            'mask': torch.tensor(mask, dtype=torch.long),
+            # 'token_type_ids': torch.tensor(token_type_ids, dtype=torch.long),
+            'targets': torch.tensor(self.targets[index], dtype=torch.float)
+        }
+
+
+# %%
+train_size = 0.8
+train_dataset = df_train.sample(frac=train_size, random_state=200).reset_index(drop=True)
+test_dataset = df_train.drop(train_dataset.index).reset_index(drop=True)
+
+
+print("FULL Dataset: {}".format(df_train.shape))
+print("TRAIN Dataset: {}".format(train_dataset.shape))
+print("TEST Dataset: {}".format(test_dataset.shape))
+
+training_set = CustomDataset(train_dataset, tokenizer, MAX_LEN)
+testing_set = CustomDataset(test_dataset, tokenizer, MAX_LEN)
+
+# %%
+train_params = {'batch_size': TRAIN_BATCH_SIZE,
+                'shuffle': True,
+                'num_workers': 8
+                }
+
+test_params = {'batch_size': VALID_BATCH_SIZE,
+                'shuffle': True,
+                'num_workers': 8
+                }
+
+training_loader = DataLoader(training_set, **train_params)
+testing_loader = DataLoader(testing_set, **test_params)
+
+# %%
+class BERTClass(torch.nn.Module):
+    def __init__(self):
+        super(BERTClass, self).__init__()
+        # self.l1 = BertModel.from_pretrained('bert-base-uncased', return_dict=False)
+        self.l1 = DistilBertModel.from_pretrained('distilbert-base-uncased', return_dict=False)
+        self.l2 = torch.nn.Dropout(0.3)
+        self.l3 = torch.nn.Linear(768, 6)
+
+    def forward(self, ids, mask,
+                # token_type_ids
+                ):
+        # _, output_1 = self.l1(ids, attention_mask=mask, token_type_ids=token_type_ids)
+        output_1 = self.l1(ids, attention_mask=mask, return_dict=False)
+
+        output_2 = self.l2(output_1)
+        output = self.l3(output_2)
+        return output
+
+
+model = BERTClass()
+model.to(device)
+
+
+# %%
+def loss_fn(outputs, targets):
+    return torch.nn.BCEWithLogitsLoss()(outputs, targets)
+
+
+optimizer = torch.optim.Adam(params=model.parameters(), lr=LEARNING_RATE)
+num_training_steps = EPOCHS * len(training_loader)
+progress_bar = tqdm(range(num_training_steps))
+# %%
+def train(epoch):
+    model.train()
+    for _, data in enumerate(training_loader, 0):
+        ids = data['ids'].to(device, dtype=torch.long)
+        mask = data['mask'].to(device, dtype=torch.long)
+        # token_type_ids = data['token_type_ids'].to(device, dtype=torch.long)
+
+        targets = data['targets'].to(device, dtype=torch.float)
+
+        outputs = model(ids,
+                        mask,
+                        # token_type_ids
+                        )
+
+        optimizer.zero_grad()
+        loss = loss_fn(outputs, targets)
+        if _ % 5000 == 0:
+            print(f'Epoch: {epoch}, Loss:  {loss.item()}')
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        progress_bar.update(1)
+
+
+for epoch in range(EPOCHS):
+    train(epoch)
+
+
+# %%
+def validation(epoch):
+    model.eval()
+    fin_targets = []
+    fin_outputs = []
+    with torch.no_grad():
+        for _, data in enumerate(testing_loader, 0):
+            ids = data['ids'].to(device, dtype=torch.long)
+            mask = data['mask'].to(device, dtype=torch.long)
+            # ids = ids.unsqueeze(0)
+            # token_type_ids = data['token_type_ids'].to(device, dtype = torch.long)
+            targets = data['targets'].to(device, dtype=torch.float)
+            outputs = model(ids,
+                            mask,
+                            # token_type_ids
+                            )
+            fin_targets.extend(targets.cpu().detach().numpy().tolist())
+            fin_outputs.extend(torch.sigmoid(outputs).cpu().detach().numpy().tolist())
+    return fin_outputs, fin_targets
+
+
+for epoch in range(EPOCHS):
+    outputs, targets = validation(epoch)
+    outputs = np.array(outputs) >= 0.5
+    accuracy = metrics.accuracy_score(targets, outputs)
+    f1_score_micro = metrics.f1_score(targets, outputs, average='micro')
+    f1_score_macro = metrics.f1_score(targets, outputs, average='macro')
+    print(f"Accuracy Score = {accuracy}")
+    print(f"F1 Score (Micro) = {f1_score_micro}")
+    print(f"F1 Score (Macro) = {f1_score_macro}")
+
